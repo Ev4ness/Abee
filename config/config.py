5847087243f2dba7d0b94323bb5ad79e2ca3212b
Vooyage <148488760+Vygaee@users.noreply.github.com>
2023-11-17 23:16:13 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -61,7 +61,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/kenkansaja/Musikku",
+    "https://t.me/TopMutualanGrup",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Musikku")
 
